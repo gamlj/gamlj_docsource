ac0ed39f4c4659053dbb038abc4ba4e38b6338f2
Marcello Gallucci <mcfanda@gmail.com>
2018-08-03 19:31:02 +0200

    updates for mixed

---
+++ R/tests.R
@@ -0,0 +1,10 @@
+### mixed 1 ###
+dat<-read.csv2("data/beers_bars.csv")
+dat$smile<-as.numeric(as.character(dat$smile))
+dat$beer<-as.numeric(as.character(dat$beer))
+dat$beer<-as.numeric(scale(dat$beer,scale = F))
+library(lmerTest)
+model<-lmer(smile~beer+(1|bar),data=dat)
+summary(model)
+model<-lmer(smile~beer+(1+beer|bar),data=dat)
+summary(model)
