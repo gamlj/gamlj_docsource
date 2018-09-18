aa2ac7b53141c7f722e51b76169d14539ffb1161
Marcello Gallucci <mcfanda@gmail.com>
2018-09-18 18:53:43 +0200

    towards version 1.0.0

--- R/tests.R
+++ R/tests.R
@@ -4,7 +4,11 @@ dat$smile<-as.numeric(as.character(dat$smile))
 dat$beer<-as.numeric(as.character(dat$beer))
 dat$beer<-as.numeric(scale(dat$beer,scale = F))
 library(lmerTest)
-model<-lmer(smile~beer+(1|bar),data=dat)
+model<-lmer(smile~beer+(1|bar),data=dat,REML = F)
 summary(model)
 model<-lmer(smile~beer+(1+beer|bar),data=dat)
 summary(model)
+
+anova(model, type=2, ddf="Kenward-Roger")
+
+ranova(model)
