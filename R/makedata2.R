33599bc3402c15b923638d173bcdc6318ce4c8a8
Marcello Gallucci <mcfanda@gmail.com>
2018-12-04 15:48:54 +0100

    spelling

--- R/makedata2.R
+++ R/makedata2.R
@@ -1,3 +1,4 @@
+### crossed stimuli ###
 nsubj<-50
 nstimuli<-30
 asub<-rnorm(nsubj,0,2) # subjects intercepts
@@ -6,7 +7,6 @@ fac<-c(-1,1)
 bfac<-rnorm(nsubj,.4,1)
 dat<-NULL
 a<-20
-i<-i
 j<-1
 for (i in 1:nsubj) {
   for (j in 1:nstimuli) {
@@ -33,47 +33,47 @@ dat<-dat[,c("y","cond","subj","stimulus")]
 model<-lmer(y~(1+cond|subj)+(1|stimulus)+cond,data=dat)
 summary(model)
 
+### nested stimuli ###
 
 nsubj<-50
-nstimuli<-60
-spc<-nstimuli/2
+nstimuli<-36
 asub<-rnorm(nsubj,0,2) # subjects intercepts
 asti<-rnorm(nstimuli,0,2) # stimuli intercepts
-fac<-c(-1,1)
-ifac<-c(0,1)
-bfac<-rnorm(nsubj,.4,1)
+fac<-c(0,1)
+b<-1.9
+bfac<-rnorm(nsubj,b,1)
 dat<-NULL
 a<-20
-i<-i
 j<-1
+
 for (i in 1:nsubj) {
-  for (k in ifac) {
-  start<-(spc*k)+1
-  end<-(spc*k)+spc
+  for (k in 0:(length(fac)-1)) {
+  start<-(nstimuli*k/2)+1
+  end<-(nstimuli*(1+k))/2
+  x<-k-.5
   for (j in start:end) {
-    x<-fac[k+1]
     aj<-asti[j]
     ai<-asub[i]
     bi<-bfac[i]
-    print(length(x))
     y<-a+ai+aj+bi*x+rnorm(length(x),0,3)
-    dat<-rbind(dat,cbind(y,x,i,j))
-  }
+    dat<-rbind(dat,cbind(y,k,i,j))
   }
 }
+}
 dat<-as.data.frame(dat)
-dat
+table(dat$i)
+
 library(lmerTest)
 dat$subj<-factor(dat$i)
 dat$stimulus<-factor(dat$j)
-dat$cond<-factor(dat$x)
+dat$cond<-factor(dat$k)
 model<-lmer(y~(1+cond|subj)+(1|stimulus)+cond,data=dat)
 summary(model)
 contrasts(dat$cond)<-contr.sum(2)
+names(dat)
 dat<-dat[,c("y","cond","subj","stimulus")]
-#write.csv(dat,"data/subjects_by_stimuli.csv")
+write.csv(dat,"data/subjects_on_stimuli.csv")
 
 model<-lmer(y~(1+cond|subj)+(1|stimulus)+cond,data=dat)
 summary(model)
 table(dat$cond,dat$stimulus)
-936+392
