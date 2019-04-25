d39e693edfb88bbbe5136e16c1ce171c568e067e
Marcello Gallucci <mcfanda@gmail.com>
2019-04-25 16:41:14 +0200

    updates

--- R/install.R
+++ R/install.R
@@ -1,10 +1,14 @@
 library(rmarkdown)
+SOURCE_FOLDER="../pubs"
+TARGET_FOLDER="../gamlj.github.io"
 msg<-"updates"
 render_site("docssource/")
 system("git add .")
 system(paste('git commit -m "',msg,'"'))
 system("git push origin master")
 
+cmd<-paste("cp -R ",paste0(SOURCE_FOLDER,"/*"),paste0(TARGET_FOLDER,"/"))
+system("cp ")
 
 
 
