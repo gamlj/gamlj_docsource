9ef534acb60427827966e618c1f07841903f856c
Marcello Gallucci <mcfanda@gmail.com>
2019-04-25 18:42:08 +0200

    updates

--- R/constants.R
+++ R/constants.R
@@ -1,6 +1,8 @@
 # folders should be absolute or relative to the docssource folder
+# do not use the trailing slash
 MODULE_FOLDER="../../gamlj"
 SOURCE_FOLDER="pubs"
 TARGET_FOLDER="../gamlj.github.io"
 DATALINK<-"https://raw.githubusercontent.com/mcfanda/gamlj_docs/master/data"
+## DATALINK can be a 
 DATALINK<-"data"
