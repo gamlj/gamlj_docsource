31b5d90076b39f3e9a5887f857ad681086d52779
Marcello Gallucci <mcfanda@gmail.com>
2019-04-25 18:55:02 +0200

    updates

--- R/functions.R
+++ R/functions.R
@@ -4,6 +4,15 @@ library(Rsearchable)
 
 source("../R/constants.R")
 
+localdatafolder<-function() {
+  ishttps<-(length(grep("https:/",DATALINK,fixed = T))>0)
+  ishttp<-(length(grep("http:/",DATALINK,fixed = T))>0)
+  if (ishttp || ishttps)
+    return(FALSE)
+  return(DATALINK) 
+}
+
+
 datafile<-function(name,file) {
   if (length(grep(":/",file,fixed = T))==0)
     file<-paste0(DATALINK,"/",file)
